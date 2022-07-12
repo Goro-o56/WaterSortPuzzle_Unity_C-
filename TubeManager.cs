f6b30b7a5693464d0d8a7062fe8bf2ac8cd6e7ad
goro-o56 <yamashitagoro2727@gmail.com>
2022-07-12 13:41:27 +0900

    処理を関数化して呼び出しやすくした

--- TubeManager.cs
+++ TubeManager.cs
@@ -17,105 +17,151 @@ public class TubeManager : MonoBehaviour
     void Start()
     {
         //getChildren
-        children = new Transform[parent.transform.childCount];
-
-        int count = 0;
-        foreach(Transform child in parent.transform) {
-            children[count] = child;
-            Debug.Log($"{count}番目の子供は{children[count].name}です");
-            count++;
-        }
+        children = getChildren(parent);
 
         //ソート
-        float scale_y = 1.0f;
-        float Top_Y = count*scale_y/2.0f;
-        int cnt_0 = 0;
+        int  Top_Y = parent.transform.childCount;
         
-        while(cnt_0 < parent.transform.childCount){
-            Vector3 pos = transform.localPosition;
-            pos.y = Top_Y - scale_y*cnt_0;
-            children[cnt_0].transform.localPosition = pos;
+        Sort(children);
 
-            cnt_0++;
-        }
+        //チェック
+        int counter;
+        Transform[] tmpArr; 
+        counter = Check();
 
+        //tmpArrをテストする
+        Test_tmpArr(tmpArr);
 
         Debug.Log($"これはカウント{parent.transform.childCount}");
-        //childrenをチェックしていく。 配列を頭から見ていく。
-        Transform[] tmpArr;
-        tmpArr = new Transform[parent.transform.childCount];
-        //1つ1つchildrenをチェックしていく 
+
+        //圧縮配列を作る
+        Debug.Log($"{counter}の長さの配列を作ります");
+        Transform[] endArr; //要素を圧縮するための配列
+
+        endArr = compressArr(tmpArr, counter); 
+
+        reform_endArrElement(endArr);
+
+        //getChildren
+        Transform[] getChildren(GameObject parent){
+            children = new Transform[this.parent.transform.childCount];
+
+            int count = 0;
+            foreach(Transform child in this.parent.transform) {
+                children[count] = child;
+                Debug.Log($"{count}番目の子供は{children[count].name}です");
+                count++;
+            }
+
+            return children;
+        }
+
         
-        int itr = 0;
-        int counter = 0;
-        while(itr < parent.transform.childCount){
-            //一致したら配列をいれる
-            if(children[itr].tag == children[itr+1].tag) {
-                Debug.Log($"{children[itr].tag}が一致しました");
-   
-                if(itr == 0){
-                    //まず0番目をチェックする
-                    Debug.Log($"{tmpArr[itr]}に代入しています");
-                    tmpArr[itr] = children[itr];
-                    counter++;
-                    Debug.Log($"tmpArr[itr]に{children[itr]}代入しました");
+        void Sort(Transform[] children){
+            float scale_y = 1.0f;
+            float Top_Y = parent.transform.childCount;
+            int cnt = 0;
+
+            while(cnt < parent.transform.childCount){
+                Vector3 pos = transform.localPosition;
+                pos.y =  Top_Y - scale_y * cnt;
+                children[cnt].transform.localPosition = pos;
+                cnt++;
+            }
+        }
+
+        //childrenをチェックしていく
+        int Check(){
+            
+            tmpArr = new Transform[parent.transform.childCount]; //parent引き継ぐ必要あるのか…？
+            //1つ1つchildrenをチェックしていく
+            int itr = 0;
+            int counter = 0;
+
+            while(itr < parent.transform.childCount){
+                //一致したら配列を入れる
+                if(children[itr].tag == children[itr+1].tag) {
+                    Debug.Log($"{children[itr].tag}が一致しました");
+
+                    if(itr == 0) {
+                        //まず0番目をチェック
+                        Debug.Log($"{tmpArr[itr]}に代入しています");
+                        tmpArr[itr] = children[itr];
+                        counter++;
+                        Debug.Log($"tmpArr[itr]に{children[itr]}代入しました");
                     }
                 tmpArr[itr+1] = children[itr+1];
                 Debug.Log($"{children[itr+1]}を代入しました");
                 counter++;
                 itr++;
                 continue;
-            }else{
-                break;
+                }else{
+                    break;
+                };
+            }
+            return counter;
+        }
+
+        void Test_tmpArr(Transform[] tmpArr) {
+            int cnt = 0;
+
+            Debug.Log($"{counter}の長さの配列を作ります");
+
+            if(tmpArr.Length>0){
+                foreach(Transform child in tmpArr){
+                    Debug.Log($"{cnt}番目");
+                    Debug.Log($"{child}");
+                    cnt++;
+                }
             }
         }
 
-        //テスト 各tmpArrを出力する
-        int cnt_1 = 0;
-        int cnt_2 = 0;
+        Transform[] compressArr(Transform[] tmpArr,int counter){
+            endArr = new Transform[counter];
+            int cnt = 0;
+            if(tmpArr.Length > 0 ){
+                //endArrに頭から入れていく
+                while(cnt < counter){
+                    endArr[cnt] = tmpArr[cnt];
+                    cnt++;
+                }
+            }
 
-        Debug.Log($"{counter}の長さの配列を作ります");
-        Transform[] endArr;
-        endArr = new Transform[counter];
-        //endArrに頭から入れていく
-        if(tmpArr.Length > 0){
-        Debug.Log("-----");
-        while(cnt_1 < counter){
-            endArr[cnt_1] = tmpArr[cnt_1];
-            cnt_1++;
+            return endArr;
         }
+
+        void reform_endArrElement(Transform[] endArr){
+            Debug.Log($"{endArr}を整形します");
+            if(endArr.Length > 1) {
+                //endArrの0番目の大きさを変える
+                Vector3 tmp;
+                tmp = endArr[0].transform.localScale;
+                tmp.y = tmp.y * endArr.Length;
+                endArr[0].transform.localScale = tmp;
+                
+                //1から最後までの要素を削除する
+                int cnt_1 = 1; //endArr要素を指定するインデックス
+                int cnt_2 = 0; //削除した回数
+
+                while(cnt_1 < endArr.Length){
+                    Destroy(endArr[cnt_1].gameObject);
+                    cnt_2++;
+                    cnt_1++;
+                }
+
+                Vector3 tmp_1;
+                tmp_1 = endArr[0].transform.localPosition;
+                tmp_1.y = Top_Y - 0.5f * cnt_2;
+                endArr[0].transform.localPosition = tmp_1;
+            }
+            
+        };
+
 
         
-        //endArrのテスト
-        foreach(Transform child in endArr){
-            
-            Debug.Log($"{cnt_2}番目");
-            Debug.Log($"{child}");
-            cnt_2++;
-        }
-        Debug.Log("-----");
-        }
 
-        if(endArr.Length > 1) {
-            //endArrの0番目の大きさを変える
-            Vector3 tmp;
-            tmp = endArr[0].transform.localScale;
-            tmp.y = tmp.y * endArr.Length; 
-            endArr[0].transform.localScale = tmp;
-            //1から最後までの要素を削除する
-            int cnt_3 = 1; 
-            int cnt_4 = 0; //削除した回数
-            while(cnt_3 < endArr.Length){
-                Destroy(endArr[cnt_3].gameObject);//Transformは削除できない
-                cnt_4++;//一つ削除しました
-                cnt_3++;
-            }
-            //消した分*0.5 posYを下げる
-            Vector3 tmp_1;
-            tmp_1 = endArr[0].transform.localPosition;
-            tmp_1.y = Top_Y - 0.5f * cnt_4 ; 
-            endArr[0].transform.localPosition = tmp_1;
-        }
+
+
     }
 
     // Update is called once per frame
@@ -126,7 +172,27 @@ public class TubeManager : MonoBehaviour
 
 }
 
+public class Tube{ //クラス作ってみたが、アタッチされていないので呼び出せない
+    public GameObject parent;
 
+    Transform[] children;
+    int count;
+
+    public Tube (GameObject Parent, Transform[] Children){
+        parent = Parent;
+        children = Children;
+        count = 0;
+    }
+
+    public void getChildren(){
+        foreach(Transform child in parent.transform){
+            children[count] = child;
+            Debug.Log($"{count}番目の子供は{children[count].name}です");
+            count++;
+        }
+    }
+    
+}
 
 //子オブジェクトの存在を確認するクラス
 public static partial class TransformExtensions {
